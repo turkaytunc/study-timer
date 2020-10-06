81cfe5094105d94d66eb4b229779b38e7c06bcd6
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-06 14:59:23 +0300

    add BreakSection component

---
+++ src/components/BreakSection.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const BreakSection = ({ s, handleLength }) => {
+  return (
+    <div className="break-section">
+      <label id="break-label">Break Length</label>
+      <div className="break-button-container">
+        <button
+          id="break-decrement"
+          onClick={() => handleLength('break', 'decrement')}
+          disabled={s.isPlaying}
+        >
+          -
+        </button>
+        <div id="break-length">{s.breakLength}</div>
+        <button
+          id="break-increment"
+          onClick={() => handleLength('break', 'increment')}
+          disabled={s.isPlaying}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
