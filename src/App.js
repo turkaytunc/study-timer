32b1563b9913aa00fac554bd60732412de9d30d1
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-06 15:08:06 +0300

    add components together

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './App.scss';
+import BreakSection from './components/BreakSection/BreakSection';
+import SessionSection from './components/SessionSection/SessionSection';
+import DisplaySection from './components/DisplaySection/DisplaySection';
 
 function App() {
-  return <div className="App">Learn React</div>;
+  const initialState = {
+    minuteLeft: 25,
+    secondLeft: 0,
+    sessionLength: 25,
+    breakLength: 5,
+    isPlaying: false,
+    isBreak: true,
+  };
+  const [state, setState] = React.useState(initialState);
+
+  return (
+    <div className="app">
+      <div className="prog-title">Study Timer</div>
+      <div className="section-container">
+        <BreakSection s={state} />
+        <SessionSection s={state} />
+      </div>
+      <DisplaySection s={state} />
+    </div>
+  );
 }
 
 export default App;
