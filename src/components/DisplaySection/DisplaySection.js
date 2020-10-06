e70e86a38b09d38d85e90e7a5bb3414b5283f027
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-06 15:03:54 +0300

    add DisplaySection component

---
+++ src/components/DisplaySection/DisplaySection.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const DisplaySection = ({ s, handleReset, startStop }) => {
+  return (
+    <div className="display-section">
+      <div className="display-timer-container">
+        <label id="timer-label">
+          {s.isBreak ? 'Break Time' : 'Study Time'}
+        </label>
+        <div id="time-left">
+          {' '}
+          {s.minuteLeft < 10 ? '0' + s.minuteLeft : s.minuteLeft} :{' '}
+          {s.secondLeft < 10 ? '0' + s.secondLeft : s.secondLeft}{' '}
+        </div>
+      </div>
+      <div className="display-button-container">
+        <button id="start_stop" onClick={() => startStop()}>
+          Start-Stop
+        </button>
+        <button id="reset" onClick={() => handleReset()}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
