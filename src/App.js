0027cfeccb694d6099490d055ba2a9c2acf1f7ac
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-06 15:10:12 +0300

    add decrement second

--- src/App.js
+++ src/App.js
@@ -15,14 +15,59 @@ function App() {
   };
   const [state, setState] = React.useState(initialState);
 
+  let tm = null;
+
+  const decrementSecond = () => {
+    tm = setTimeout(() => {
+      if (state.secondLeft > 0 && state.isPlaying) {
+        setState({
+          ...state,
+          secondLeft: state.secondLeft - 1,
+        });
+      } else if (
+        state.secondLeft <= 0 &&
+        state.minuteLeft > 0 &&
+        state.isPlaying
+      ) {
+        setState({
+          ...state,
+          minuteLeft: state.minuteLeft - 1,
+          secondLeft: 59,
+        });
+      } else if (
+        state.minuteLeft <= 0 &&
+        state.secondLeft <= 0 &&
+        state.isPlaying
+      ) {
+        if (state.isBreak) {
+          setState({
+            ...state,
+            minuteLeft: state.sessionLength,
+            isBreak: false,
+          });
+        } else {
+          setState({
+            ...state,
+            minuteLeft: state.breakLength,
+            isBreak: true,
+          });
+        }
+      }
+    }, 1000);
+  };
+
   return (
     <div className="app">
       <div className="prog-title">Study Timer</div>
       <div className="section-container">
-        <BreakSection s={state} />
-        <SessionSection s={state} />
+        <BreakSection s={state} handleLength={handleLength} />
+        <SessionSection s={state} handleLength={handleLength} />
       </div>
-      <DisplaySection s={state} />
+      <DisplaySection
+        s={state}
+        handleReset={handleReset}
+        startStop={startStop}
+      />
     </div>
   );
 }
