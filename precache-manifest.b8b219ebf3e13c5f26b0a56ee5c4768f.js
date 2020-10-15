afc273f439cf043ed449a3d169f89170663d55dc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-15 06:03:27 +0000

    Deploy turkaytunc/study-timer to github.com/turkaytunc/study-timer.git:gh-pages

---
+++ precache-manifest.b8b219ebf3e13c5f26b0a56ee5c4768f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e5958c4cde9306b22e8e21540d7bd5",
+    "url": "/study-timer/index.html"
+  },
+  {
+    "revision": "c4dba11ba19b01020da8",
+    "url": "/study-timer/static/css/main.0f3949c8.chunk.css"
+  },
+  {
+    "revision": "886b882e2e72811b6020",
+    "url": "/study-timer/static/js/2.6f6f7a58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/study-timer/static/js/2.6f6f7a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4dba11ba19b01020da8",
+    "url": "/study-timer/static/js/main.f2eada43.chunk.js"
+  },
+  {
+    "revision": "cfec5a45ad57fc5c2466",
+    "url": "/study-timer/static/js/runtime-main.2be497e7.js"
+  }
+]);
